637afeb9c2dafffb475ff788fb266069e9869c88
MichaelJulian <mikeyjmikeyj@gmail.com>
2016-04-16 16:19:43 -0700

    authenticate users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,41 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!  
+  before_action :set_user, only: [:edit_profile, :profile, :update, :destroy]
+
   def index
+    @users = User.all
   end
 
-  def edit_profile
+  def show
+    @user=User.find(params[:id])
   end
 
-  def profile
+  def edit
+    @user = current_user
+  end
+
+  def update
+    if @user.update(users_params)
+      respond_to do |format|
+        format.html { redirect_to users_path }
+      end
+    else
+      redirect_to edit_user_path(@user)
+    end
   end
 
   def friends
+    # @friends = current_user.friendships.where(state: "ACTIVE").map(&:friend) + current_user.inverse_friendships.where(state: "ACTIVE").map(&:user)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def users_params
+    params.require(:user).permit(:first_name, :last_name, :bio, :major, :year, :preference, goals: [], goal_ids: [])
   end
 
 end
