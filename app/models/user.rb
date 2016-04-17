07b0df6c44a05ee04f011dc64062f71e7b88904b
MichaelJulian <mikeyjmikeyj@gmail.com>
2016-04-16 19:50:37 -0700

    add friendship relation, user relation, tinderslide actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,29 @@
 class User < ActiveRecord::Base
+  
+  has_many :friendships, dependent: :destroy
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def request_match(user2)
+    self.friendships.create(friend: user2)
+  end
+
+  def accept_match(user2)
+    self.friendships.where(friend: user2).first.update_attribute(:state, "ACTIVE")
+  end
+
+  def remove_match(user2)
+    inverse_friendship = inverse_friendships.where(user_id: user2).first
+    if inverse_friendship
+      self.inverse_friendships.where(user_id: user2).first.destroy
+    else
+      self.friendships.where(friend_id: user2).first.destroy
+    end
+  end
+
+
 end
